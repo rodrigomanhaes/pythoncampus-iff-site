4d3eca7fef366877dbe45325fdfb3a8d7a08c1ff
Rodrigo Manhães <rmanhaes@gmail.com>
2011-08-03 16:01:15 -0300

    open house

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pythoncampus::Application.config.secret_token = 'fc29ce6ffae6639f5b8c25db5dcd5999f6a7d5142b8bd5bdf99f192861a5a2740491a24590778acb867a7b340eefd936d026fb61f905d3e376592454d9e66a9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pythoncampus::Application.config.session_store :cookie_store, :key => '_pythoncampus_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pythoncampus::Application.config.session_store :active_record_store
