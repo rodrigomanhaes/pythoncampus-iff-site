73e26c2e6f01ac9e4a326d2b9247aeb0c48f203c
Rodrigo Manhães <rmanhaes@gmail.com>
2011-08-05 02:13:04 -0300

    attendee's e-mail format validation

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,5 +3,8 @@ class Attendee < ActiveRecord::Base
   has_many :presentations, :through => :registrations
 
   validates_presence_of :name, :email
+  validates_format_of :email,
+                      :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                      :allow_blank => true
 end
 
